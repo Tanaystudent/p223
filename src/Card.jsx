d6d1740a1258db64309f91bb1cd36d3e2fd537be
Tanaystudent <133126644+Tanaystudent@users.noreply.github.com>
2024-02-11 23:32:04 +0530

    Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,7 +5,7 @@ function Card(props)
     <div className="column">
     <img src= {props.avatar} alt ='Staff' width ={200} height ={200}/>
     <h3>{props.name} </h3>
-    <p> {props. position}</p>
+    <p> {props.position}</p>
 
     </div>
    )
